c50350af2bf17e27a597b0ab3cda13e33f7d5325
CaioLLima <caiololima2@gmail.com>
2025-04-08 20:38:27 -0300

    Add project files.

---
+++ DiamanteDeX/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiamanteDeX
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Trabalho 01 - Diamante de X");
+        }
+    }
+}
