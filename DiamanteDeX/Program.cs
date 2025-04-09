5829ae2040b3b3d8af2ea14dd43538a3c472d380
CaioLLima <caiololima2@gmail.com>
2025-04-08 21:16:06 -0300

    Inserido a opção de inserir um número novamente caso seja uma entrada inválida.

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -1,12 +1,37 @@
-﻿namespace DiamanteDeX
+﻿using System.Collections;
+
+namespace DiamanteDeX
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Trabalho 01 - Diamante de X");
-            Console.Write("Insira um número ímpar inteiro: ");
-            int entrada = int.Parse(Console.ReadLine());
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Trabalho 01 - Diamante de X");
+                Console.Write("Insira um número ímpar inteiro: ");
+                int entrada = int.Parse(Console.ReadLine());
+
+                if (entrada % 2 != 0)
+                {
+
+
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Entrada inválida, insira um número novamente.");
+                    Console.ReadLine();
+                    continue;
+                    
+                }
+                   
+
+                Console.ReadLine();
+            }
+            
         }
     }
 }
