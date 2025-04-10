fac0ae3c6b48cc4a61f5ece27c59068693ef00e0
CaioLLima <caiololima2@gmail.com>
2025-04-10 13:02:48 -0300

    Loop para percorrer as linhas

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -14,36 +14,12 @@ namespace DiamanteDeX
                 int entrada = 5;//int.Parse(Console.ReadLine());
                 string[] diamante = new string[entrada];
                 int meio = entrada / 2;
-                int esquerda = meio;
-                int direita = meio;
 
                 if (entrada % 2 != 0 && entrada > 1)
                 {
                     for (int i = 0; i < entrada; i++)
                     {
-                        string linha = "";
-                        for (int j = 0; j < entrada; j++)
-                        {                         
-                            if (j == direita || j == esquerda) // cria grande vertical de X
-                            {
-                                linha += "X";
-                            }
-                            else
-                                linha += "_";
-                        }
-
-                        Console.Write(linha);
-
-                        if (i < meio) 
-                        {
-                            esquerda--;
-                            direita++;
-                        }
-                        else
-                        {
-                            esquerda++;
-                            direita--;
-                        }
+                        
 
                         Console.WriteLine(""); // faz separação das linhas
                     }
