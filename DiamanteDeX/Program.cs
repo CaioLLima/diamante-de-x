c998f4e63478770e6db545f7878a04f43bb3b307
CaioLLima <caiololima2@gmail.com>
2025-04-09 22:20:53 -0300

    Grande horizontal e grande vertical produzindo os X's indepedente do número ímpar.

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -13,7 +13,6 @@ namespace DiamanteDeX
                 Console.WriteLine("Insira um número ímpar inteiro: ");
                 int entrada = 5;//int.Parse(Console.ReadLine());
                 string[] diamante = new string[entrada];
-                string linha1 = "";
 
                 if (entrada % 2 != 0 && entrada > 1)
                 {
@@ -22,44 +21,32 @@ namespace DiamanteDeX
                         for (int j = 0; j < entrada; j++)
                         {                         
                             diamante[j] = " "; //transforma o 5 x 5 em _
+                            diamante[entrada/2] = "X"; //está criando os X na vertical principal entrada/2
 
-                            diamante[2] = "X"; //está criando os X na vertical principal entrada/2
-
-                            if (i==1)
-                            {
-                                diamante[entrada/2 -1] = "X";
-                                diamante[entrada / 2 + 1] = "X";
-                            }
-                            if (i == 2)
+                           /* if (i == 1)
                             {
                                 diamante[entrada / 2 - 1] = "X";
                                 diamante[entrada / 2 + 1] = "X";
-                                diamante[entrada / 2 - 2] = "X";
-                                diamante[entrada / 2 + 2] = "X";
+                            }*/
+                            if (i == entrada / 2) //cria X na grande horizontal
+                            {
+                                for (int p = 0; p < entrada; p++) 
+                                {
+                                    diamante[p] = "X";
+                                }
+                                
                             }
-                            if (i == 3)
+                          /*  else if (i == 3)
                             {
                                 diamante[entrada / 2 - 1] = "X";
                                 diamante[entrada / 2 + 1] = "X";
-                            }
-                            
+                            }*/
 
-
-
-                            //diamante[entrada - 1] = "X";
-                            Console.Write(diamante[j]);
-                            
+                            Console.Write(diamante[j]);  
                         }
-                        diamante[i] = "C";
-                       // diamante[entrada / 2] = "X";
 
-                        
                         Console.WriteLine(""); // faz separação das linhas
-
                     }
-
-
-
                 }
                 else
                 {
@@ -67,8 +54,6 @@ namespace DiamanteDeX
                     Console.ReadLine();
                     continue;            
                 }
-                   
-
                 Console.ReadLine();
             }
             
