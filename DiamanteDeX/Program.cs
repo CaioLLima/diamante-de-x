7272e5f9e6ec59c82de6c7e7c830f47996bfea15
CaioLLima <caiololima2@gmail.com>
2025-04-11 13:07:24 -0300

    Criando código estruturado

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -1,57 +1,91 @@
 ﻿using System.Collections;
+using System.Threading.Tasks;
 
 namespace DiamanteDeX
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             while (true)
             {
-                Console.Clear();
-                Console.WriteLine("Trabalho 01 - Diamante de X");
-                Console.WriteLine("Insira um número ímpar inteiro: ");
-                int entrada = int.Parse(Console.ReadLine());
-              
+                //cabeçalho
+                MostrarCabeçalho();
+
+                //obter numero
+                int entrada = ObterNumero();
+
+
+                //processamento
                 if (entrada % 2 != 0 && entrada >= 1) // não pode ser par e não pode ser menor ou igual a um
                 {
-                    for (int i = 0; i < entrada; i++)
-                    {
-                        int numX; // número de X por linha
-
-                        if (i <= entrada / 2)
-                        {
-                            numX = i * 2 + 1; // Indo de cima para o meio
-                        }
-                        else
-                        {
-                            numX = (entrada - i - 1) * 2 + 1; // Indo do  meio para baixo
-                        }
-
-                        int espacos = (entrada - numX) / 2;
-
-                        for (int j = 0; j < espacos; j++) //Criando espaços em branco
-                        {
-                            Console.Write(" ");
-                        }
-
-                        for (int j = 0; j < numX; j++)
-                        {
-                            Console.Write("X"); //imprimindo X baseado no número de linhas
-                        }
-
-                        Console.WriteLine(""); // faz separação das linhas
-                    }
+                    //gera linhas
+                    CalculaX(entrada);
                 }
                 else
                 {
-                    Console.WriteLine("Entrada inválida, insira um número novamente.");
+                    Console.WriteLine("Entrada inválida, tente novamente.");
                     Console.ReadLine();
-                    continue;            
                 }
+
                 Console.ReadLine();
+            }   
+        }
+
+        static void MostrarCabeçalho()
+        {
+            Console.Clear();
+            Console.WriteLine("Trabalho 01 - Diamante de X");
+        }
+
+        static int ObterNumero()
+        {
+            Console.WriteLine("Insira um número ímpar inteiro: ");
+            int entrada = 5;// int.Parse(Console.ReadLine());
+            return entrada;
+        }
+
+        static int CalculaX(int entrada)
+        {
+            int numX = 0; // número de X por linha
+            for (int i = 0; i < entrada; i++)  //loop principal para gerar linhas
+            {          
+                //calculo desenho
+                if (i <= entrada / 2) //se a linha for menor que a metade do diamante
+                {
+                    numX = i * 2 + 1; // calculo de quantos X vai ter na linha
+                }
+                else //se a linha for maior que a metade do diamante
+                {
+                    numX = (entrada - i - 1) * 2 + 1; // Indo do  meio para baixo
+                }
+                //calcula espaços
+                int espacos = EspacosBrancos(numX, entrada);
+                MostrarDesenho(numX, espacos);
             }
-            
+            return numX;
+        }
+
+        static int EspacosBrancos(int numX, int entrada)
+        {
+            int espacos = (entrada - numX) / 2; //calculo saber quantos espaços em branco  vai ter
+            return espacos;
+        }
+
+        static void MostrarDesenho(int numX, int espacos)
+        {
+            //mostrar desenho
+            for (int j = 0; j < espacos; j++) //Criando espaços em branco
+            {
+                Console.Write(" ");
+            }
+
+            for (int j = 0; j < numX; j++)
+            {
+                Console.Write("X"); //imprimindo X baseado no número de X que vai ter na linha
+            }
+
+            Console.WriteLine(""); // faz separação das linhas
         }
     }
 }
