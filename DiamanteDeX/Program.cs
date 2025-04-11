549824686384df3a80e9adee3910f097747fb98c
CaioLLima <caiololima2@gmail.com>
2025-04-11 13:13:12 -0300

    Remoção dos comentários do código main

--- DiamanteDeX/Program.cs
+++ DiamanteDeX/Program.cs
@@ -13,34 +13,34 @@ namespace DiamanteDeX
                 Console.WriteLine("Insira um número ímpar inteiro: ");
                 int entrada = int.Parse(Console.ReadLine());
               
-                if (entrada % 2 != 0 && entrada >= 1) // não pode ser par e não pode ser menor ou igual a um
+                if (entrada % 2 != 0 && entrada >= 1)
                 {
                     for (int i = 0; i < entrada; i++)
                     {
-                        int numX; // número de X por linha
+                        int numX;
 
                         if (i <= entrada / 2)
                         {
-                            numX = i * 2 + 1; // Indo de cima para o meio
+                            numX = i * 2 + 1;
                         }
                         else
                         {
-                            numX = (entrada - i - 1) * 2 + 1; // Indo do  meio para baixo
+                            numX = (entrada - i - 1) * 2 + 1;
                         }
 
                         int espacos = (entrada - numX) / 2;
 
-                        for (int j = 0; j < espacos; j++) //Criando espaços em branco
+                        for (int j = 0; j < espacos; j++) 
                         {
                             Console.Write(" ");
                         }
 
                         for (int j = 0; j < numX; j++)
                         {
-                            Console.Write("X"); //imprimindo X baseado no número de linhas
+                            Console.Write("X");
                         }
 
-                        Console.WriteLine(""); // faz separação das linhas
+                        Console.WriteLine(""); 
                     }
                 }
                 else
